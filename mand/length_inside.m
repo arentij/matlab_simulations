166f634dff4de547e07bc2cd2b58b832e11f7bbb
arentij <arentij@gmail.com>
2019-07-08 15:49:45 -0400

    adding mand set

--- mand/length_inside.m
+++ mand/length_inside.m
@@ -1,10 +1,11 @@
 function [N] = length_inside(c)
 %LENGTH_INSIDE Summary of this function goes here
 %   Detailed explanation goes here
-L = 100;
+L = 200;
 N = L;
 path = zeros(L,1);
 path(1) = c;
+
 for k = 2:L
     path(k) = path(k-1).^2 + c;
     if abs(path(k)) > 2
--- mand/length_inside_j.m
+++ mand/length_inside_j.m
@@ -1,16 +1,19 @@
-function [N] = length_inside(c)
+function [N] = length_inside_j(z0)
 %LENGTH_INSIDE Summary of this function goes here
 %   Detailed explanation goes here
-L = 200;
+L = 100;
+N = L;
 path = zeros(L,1);
-path(1) = c;
+path(1) = z0;
+c = -0.4 + 0.6i;
 for k = 2:L
     path(k) = path(k-1).^2 + c;
     if abs(path(k)) > 2
-        
+        N = k;
+        return
+    end
+    
 end
-for 
 
-outputArg1 = c;
 end
 
